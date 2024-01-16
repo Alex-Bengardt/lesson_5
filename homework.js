990e71e1c0924f19aad0c0e0f54ce6856c84f291
Alex Bengardt <alexandragorky@yandex.ru>
2024-01-16 12:47:22 +0300

    year is placed in array result

--- homework.js
+++ homework.js
@@ -3,15 +3,17 @@ console.log(str[0].toUpperCase() + str.slice(1));
 
 let str1 = "AbcdEfg";
 let str2 = "abCDEFg";
-console.log(str1.localeCompare(str2));
-console.log(str1.toUpperCase().localeCompare(str2.toUpperCase()));
+console.log(str1 === str2);
+console.log(str1.toUpperCase() === str2.toUpperCase());
 
 let years = [2000, 2001, 2002, 2003, 2004, 2005];
+let result = [];
 for (let year of years) {
 	if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-		console.log(year);
+		result.push(year);
 	}
 }
+console.log(result);
 
 let numbers = [6, 187, 66, 4, 67, 30, 18];
 console.log(numbers.sort());
